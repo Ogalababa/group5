02b17754c38f391872e9982b9da3e2d7411de78a
Joris <mathijsen.joris@gmail.com>
2024-04-12 15:58:23 +0200

    Feature: Added a footer

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,22 @@
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+
+export default function Footer() {
+    return (
+        <>
+            <Navbar expand="lg" className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/contact">Contact</Nav.Link>
+                            <Nav.Link href="/about-us">Over ons</Nav.Link>
+                            <Nav.Link href="/certificate">Certificering</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
+}
