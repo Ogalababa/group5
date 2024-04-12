bf8941f3150c2d8fa050307015e5558ec086becf
Joris <mathijsen.joris@gmail.com>
2024-04-12 14:44:09 +0200

    Added a Router function to switch betweeen the different pages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/app/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import HomePage from "./components/Home/HomePage";
+import AboutUsPage from "./components/AboutUs/AboutUsPage";
+import JobsPage from "./components/Jobs/JobsPage";
+import CertificatePage from "./components/Certificate/CertificatePage";
 
-
+const router = createBrowserRouter([
+    {
+        path:"/",
+        element: <HomePage/>
+    },
+    {
+        path:"/jobs",
+        element: <JobsPage/>
+    },
+    {
+        path:"/about-us",
+        element: <AboutUsPage/>
+    },
+    {
+        path:"/certificate",
+        element: <CertificatePage/>
+    }
+])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router}/>
 );
 
