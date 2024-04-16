7984b5f12dcdc2cc0c121bb63cb922caddeb9f74
Ogalababa <delay7.su@gmail.com>
2024-04-16 16:54:58 +0200

    to azure backend get api url

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -14,7 +14,7 @@ export default function Footer() {
                             <Nav.Link href="/contact">Contact</Nav.Link>
                             <Nav.Link href="/about-us">Over ons</Nav.Link>
                             <Nav.Link href="/certificate">Certificering</Nav.Link>
-                            <Nav.Link href={`api/test`}>API Test Link</Nav.Link>
+                            <Nav.Link href={`${process.env.REACT_APP_API_URL}/test`}>API Test Link</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
